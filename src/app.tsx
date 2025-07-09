72b2752e415ce4a910046a5dc60f4db40bfa3e33
Hugobertoncelo <hugobertoncelo@gmail.com>
2025-07-09 08:41:16 -0300

    nova biblioteca

--- src/app.tsx
+++ src/app.tsx
@@ -1,12 +1,19 @@
-import { Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { CreateRoom } from "./pages/create-room";
 import { Room } from "./pages/room";
 
+const queryClient = new QueryClient();
+
 export function App() {
   return (
-    <Routes>
-      <Route element={<CreateRoom />} index />
-      <Route element={<Room />} path="/room" />
-    </Routes>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<CreateRoom />} index />
+          <Route element={<Room />} path="/room/:roomId" />
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
   );
 }
